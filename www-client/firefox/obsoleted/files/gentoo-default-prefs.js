6dc7d5c9876a437231380db23ff3fb1c587fba27
none <none@i87k>
2022-03-07 19:20:05 +0100

    up to date
    more or less

---
+++ www-client/firefox/obsoleted/files/gentoo-default-prefs.js
@@ -0,0 +1,13 @@
+//pref("general.smoothScroll",               true);
+//pref("general.autoScroll",                 false);
+//pref("browser.urlbar.hideGoButton",        true);
+//pref("accessibility.typeaheadfind",        true);
+//pref("browser.shell.checkDefaultBrowser",  false);
+//pref("browser.EULA.override",              true);
+//pref("general.useragent.locale",           "chrome://global/locale/intl.properties");
+//pref("intl.locale.requested",              "");
+/* Disable DoH by default */
+//pref("network.trr.mode",                   5);
+/* Disable use of Mozilla Normandy service by default */
+//pref("app.normandy.enabled",               false);
+//XXX all of these are already set right in /usr/lib64/firefox/browser/defaults/preferences/vendor.js which is generated from /var/db/repos/localrepo/www-client/firefox-user-js/files/user.js
---
+++ www-client/firefox/obsoleted/files/gentoo-hwaccel-prefs.js
@@ -0,0 +1,4 @@
+/* Force hardware accelerated rendering due to USE=hwaccel */
+//pref("layers.acceleration.force-enabled",  true);
+//pref("webgl.force-enabled",                true);
+//no thanks!
